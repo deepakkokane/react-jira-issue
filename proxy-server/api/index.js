fb15e8a17007500af88ecea2a21f9bba845a1914
@deep455 <deepak.kokane@tracelinq.com>
2024-06-04 00:27:30 +0530

    "Remove proxy-server/index.js and update package.json script to point to api/index.js"

--- proxy-server/api/index.js
+++ proxy-server/api/index.js
@@ -39,10 +39,10 @@ app.get("/jira", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "build", "index.html"));
 });
 
 app.listen(port, () => {
